55fc2922511a4d5d8a56505e048300f6f1335cd8
Aleksei Udodov <weberdster@gmail.com>
2025-11-11 12:44:38 +0400

    removed unused provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,4 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
-    VladimirYuldashev\LaravelQueueRabbitMQ\LaravelQueueRabbitMQServiceProvider::class,
 ];
